5f2e6b8ea77df8193e4b36d990f51eca923e1f0e
tranlam <tdlam2105@gmail.com>
2018-05-16 15:52:29 +0700

    Add about, contact, cake home

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,4 +3,25 @@ $(document).ready(function() {
         orientation:'vertical',
         customClass:'vert'
     });
+
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+        margin: 10,
+        nav: true,
+        loop: true,
+        dots: false,
+        navText : ['<i class="fas fa-angle-left" aria-hidden="true"></i>','<i class="fas fa-angle-right" aria-hidden="true"></i>'],
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3,
+                margin: 5
+            },
+            1000: {
+                items: 4
+            }
+        }
+    })
 });
